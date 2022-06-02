9841c7da694d5124ece08b0836a32d2952b950f9
ilhofadel <32084870+ilhofadel@users.noreply.github.com>
2022-06-03 05:20:25 +0700

    Add files via upload

---
+++ test/hi.php
@@ -0,0 +1,51 @@
+<html>
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>DATA DIRI</title>
+    </head>
+    <body>
+      <h1 align="center">BIODATA DIRI</h1>
+      <table
+        border="1"
+        cellspacing="0"
+        cellpadding="5"
+        align="center"
+        width="800"
+      >
+        <tr align="center" bgcolor="#1fe5d5">
+          <td width="150">DATA DIRI</td>
+          <td width="400">KETERANGAN</td>
+        </tr>
+        <tr>
+          <td>Nama</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Role</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Avability</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Usia</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Lokasi</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Your Experience</td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>Email</td>
+          <td></td>
+        </tr>
+      </table>
+    </body>
+  </html>
+</html>
